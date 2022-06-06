2d55dcc2e0c9082aa74ed51edb764f4f0c2edfcc
AngeloMarti <106704716+AngeloMarti@users.noreply.github.com>
2022-06-06 23:12:57 +0200

    Stampa informazioni dal file stat

--- src.c
+++ src.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <fcntl.h>
 
 
 int main(int argc, char** argv) {
@@ -18,9 +19,23 @@ int main(int argc, char** argv) {
 	while (pdirent != NULL) {
 		char* pid = pdirent->d_name;
 		printf("%s\t", pid);
+
+		// Angelo
+		FILE* fd=fopen("/proc/"+pid+"/stat","r");
+		int unused;
+		char* command = (char*)malloc(sizeof(char));
+		char state;
+		int ppid;
+		fscanf(fd, "%d %s %c %d", &unused, command, &state, &ppid);
+		
+		printf("%c", state);
+		printf("%d", ppid);
+		printf("%s", command);
+		//fine Angelo
 		
 		pdirent = readdir(pdir);
 	}
+	//fine Lorenzo
 	
 	closedir(pdir);
 	
